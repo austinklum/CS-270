4934403b574b4cffadb3cd9752c9886c5a4d103a
Austin Klum <Austin Klum@DESKTOP-DCSDHIN>
2017-09-26 16:14:17 -0500

    Minor changes and added comments

--- Mandel/mandel.c
+++ Mandel/mandel.c
@@ -1,5 +1,5 @@
 /*
- * mandel.c
+ * mandel.c Prints out a ASCII Art style Mandelbrot set.
  *
  *  Created on: Sep 22, 2017
  *      Author: Austin Klum
@@ -8,16 +8,22 @@
 #include <stdio.h>
 #include <math.h>
 
+/*Print a character at a specific point*/
 void print_char(double x, double y);
 
 int main(int argc , char **argv){
 	double x, y;
 	double height = 80;
 	double width = 21;
+
+	/*Scale the (x,y) coordinates to fit inside the bounds*/
 	double x_scaler = 3 / (height - 1);
 	double y_scaler = 2 / (width - 1);
+
+	/*Loop through each point on the grid*/
 	for(y = -1; y <= 1; y += y_scaler){
 		for(x = -2; x <= 1; x += x_scaler){
+			/*Print a character at a specific point*/
 			print_char(x, y);
 		} /*End of row*/
 		/*Start next row*/
@@ -26,28 +32,34 @@ int main(int argc , char **argv){
 
 
 }
-
+/*Print a character at a specific point*/
 void print_char(double x, double y){
-	int i;
+	int i, max_i = 100;
 	double zr = 0, zi = 0;
-	for(i = 0; i < 100; i++){
+
+	/*Test weather or not the sequence coverage or diverges*/
+	for(i = 0; i < max_i; i++){
 		double zi2 = zi * zi;
 		double zr2 = zr * zr;
 
+		/*Compute the next real piece and next imaginary piece*/
 		double zr_next = zr2 - zi2 + x;
 		double zi_next = 2 * zr * zi + y;
 
+		/*Set the values to the next values*/
 		zi = zi_next;
 		zr = zr_next;
 
 		zr2 = zr * zr;
 		zi2 = zi * zi;
 
+		/*The sequence diverges*/
 		if(sqrt(zr2 + zi2) > 2 ){
 			break;
 		}
 	}
-	if(i == 100){
+	/*If it converges print a * */
+	if(i == max_i){
 		printf("*");
 	}else{
 		printf(" ");
--- pfact/pfact.c
+++ pfact/pfact.c
@@ -6,6 +6,7 @@
  */
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(int argc , char **argv){
 	int n = atoi(argv[1]);
